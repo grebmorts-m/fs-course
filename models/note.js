58110ea28deff2594b5e9095b2220059ce611ac9
Miklos Strömberg <miklos.stromberg@gmail.com>
2023-12-28 12:24:49 +0200

    uibuild

--- models/note.js
+++ models/note.js
@@ -5,6 +5,23 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
+const noteSchema = mongoose.Schema({
+  content: {
+      type: String,
+      minlength: 5,
+      required: true,
+      unique: true,
+  },
+  important: Boolean
+})
+
+noteSchema.set('toJSON', {
+transform: (document, returnedObject) => {
+  returnedObject.id = returnedObject._id.toString()
+  delete returnedObject._id
+  delete returnedObject.__v
+}
+})
 
 console.log('connecting to', url)
 mongoose.connect(url)
@@ -15,24 +32,5 @@ mongoose.connect(url)
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
-
-const noteSchema = mongoose.Schema({
-    content: {
-        type: String,
-        minlength: 5,
-        required: true
-    },
-    important: Boolean
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-
 
 module.exports = mongoose.model('Note', noteSchema)
